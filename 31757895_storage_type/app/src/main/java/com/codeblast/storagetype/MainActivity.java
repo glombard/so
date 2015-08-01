ccf0a940d90b0d66cae12ca9c46c8d3f3dafb11c
Gert Lombard <gert.lombard@gmail.com>
2015-08-01 12:03:41 -0700

    experimenting with external storage

--- 31757895_storage_type/app/src/main/java/com/codeblast/storagetype/MainActivity.java
+++ 31757895_storage_type/app/src/main/java/com/codeblast/storagetype/MainActivity.java
@@ -1,16 +1,43 @@
 package com.codeblast.storagetype;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // check for possible getExternalStorageState() constants on Environment class:
+        // MEDIA_UNKNOWN
+        // MEDIA_REMOVED
+        // MEDIA_UNMOUNTED
+        // MEDIA_CHECKING
+        // MEDIA_NOFS
+        // MEDIA_MOUNTED             <--- SD card?
+        // MEDIA_MOUNTED_READ_ONLY   <--- SD card?
+        // MEDIA_SHARED
+        // MEDIA_BAD_REMOVAL
+        // MEDIA_UNMOUNTABLE
+        Log.d(TAG, "getExternalStorageState: " + Environment.getExternalStorageState());
+        Log.d(TAG, "isExternalStorageEmulated: " + Environment.isExternalStorageEmulated());
+        Log.d(TAG, "isExternalStorageRemovable: " + Environment.isExternalStorageRemovable());
+        Log.d(TAG, "getExternalCacheDir: " + getExternalCacheDir());
+        Log.d(TAG, "getExternalFilesDir: " + getExternalFilesDir(null));
+
+        /*
+        The conclusion I've reached: the above returns the same values for my phone with a physical SD card
+        installed, and an emulator with no SD, so this is not reliable. It's because your PRIMARY
+        "external storage" isn't necessary physically external, it could be the device's own memory...
+         */
     }
 
     @Override
